f6ff9c866cd289b7ce2679d4d5ca7428e77c89ab
Zdravko Mavkov <mavkov@hotmail.com>
2019-04-30 19:40:16 +0200

    working on displaying error if no medicine found

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { newMedicine } from './newMedicine';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class DataService {
 
   receiveUrl = 'http://localhost:8080/receive';
   useUrl = 'http://localhost:8080/use';
+  errorMessage;
 
   constructor(private http: HttpClient) { }
 
@@ -24,12 +26,48 @@ export class DataService {
   addMedicine(medicine) {
     return this.http.post(this.receiveUrl, medicine)
       .subscribe(data => {
-        console.log("POST Request is successful", data);
+        console.log('POST Request for addMedicine is successful', data);
       },
         error => {
-          console.log("Error", error);
+          console.log('Error with addMedicine', error);
         }
       );
   }
+
+  useMedicine(medicine) {
+    return this.http.post(this.useUrl, medicine)
+      .subscribe(data => {
+        console.log('POST Request for useMedicine is successful', data);
+      },
+        error => {
+          console.log('Error with useMedicine', error);
+          this.errorMessage = error.error;
+          console.log('Error Message:', this.errorMessage);
+        }
+      );
+  }
+
+  // useMedicine(medicine) {
+  //   return this.http.post(this.useUrl, medicine)
+  //     .pipe(
+  //       catchError(this.handleError)
+  //     );
+  // }
+
+  // private handleError(error: HttpErrorResponse) {
+  //   if (error.error instanceof ErrorEvent) {
+  //     // A client-side or network error occurred. Handle it accordingly.
+  //     console.error('An error occurred:', error.error.message);
+  //   } else {
+  //     // The backend returned an unsuccessful response code.
+  //     // The response body may contain clues as to what went wrong,
+  //     console.error(
+  //       `Backend returned code ${error.status}, ` +
+  //       `body was: ${error.error}`);
+  //   }
+  //   // return an observable with a user-facing error message
+  //   return throwError(
+  //     'Something bad happened; please try again later.');
+  // };
 
 }
--- src/app/use/use.component.ts
+++ src/app/use/use.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-use',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UseComponent implements OnInit {
 
-  constructor() { }
+  // useMed$;
+
+  useForm = new FormGroup({
+    medicineName: new FormControl(''),
+    expirationDate: new FormControl(''),
+    patientName: new FormControl(''),
+    dateOfAdministration: new FormControl('')
+
+  });
+
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    // console.log(this.useForm.value);
+    this.useMedicine();
+    // console.log('this.blah: ', this.useMed$);
+    // console.log('errorMessage:', this.useMed$.error);
+  }
+
+  useMedicine(){
+    this.data.useMedicine(this.useForm.value);
   }
 
 }
